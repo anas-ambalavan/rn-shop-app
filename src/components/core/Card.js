113b3a93f72866fbed308d1064cf2923f02823b4
Anas <17gcs23@meaec.edu.in>
2023-02-22 16:44:05 +0530

    ProductOverviewScreen+ ,cmps=>Card, productItem

---
+++ src/components/core/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const Card = props => {
+  return <View style={{...styles.card, ...props.style}}>{props.children}</View>;
+};
+
+export default Card;
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: {height: 2, width: 0},
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+  },
+});
